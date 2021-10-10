a38fc762d17101fbdd194c5a2cf948994a924a43
SantiagoAnzola <est.santiago.anzola@unimilitar.edu.co>
2021-10-10 16:54:58 -0500

    Signed-off-by: SantiagoAnzola <est.santiago.anzola@unimilitar.edu.co>

--- Pre-parcial.js
+++ Pre-parcial.js
@@ -47,6 +47,8 @@ function cubo(x, y, z, color, material, alambrado) {
   scene.add(cube);
   return cube;
 }
+
+
 function init() {
   // create a scene, that will hold all our elements such as objects, cameras and lights.
 
@@ -72,30 +74,27 @@ function init() {
   // position the cube
   dim = 5;
   delta = 9;
-  angulo=Math.PI/2;
+  angulo = Math.PI / 2;
   Cubo = []; // Definir un array unidimensional
-
 
   Cubo.push(cubo(dim, dim, dim, 0x00ff7f, "Standard", false));
   Cubo.push(cubo(dim, dim, dim, 0xffdd00, "Physical", false));
-  Cubo.push(cubo(dim, dim, dim, 0xE80000, "Physical", false));
+  Cubo.push(cubo(dim, dim, dim, 0xe80000, "Physical", false));
 
-  Cubo[0].rotateZ(angulo);//Metodo rotacion en el eje Z
-  Cubo[0].translateX(delta); 
-  Cubo[1].rotateX(angulo);//Metodo rotacion en el eje X
+  Cubo[0].rotateZ(angulo); //Metodo rotacion en el eje Z
+  Cubo[0].translateX(delta);
+  Cubo[1].rotateX(angulo); //Metodo rotacion en el eje X
   Cubo[1].translateY(delta);
-  Cubo[2].rotateY(angulo);//Metodo rotacion en el eje Y
+  Cubo[2].rotateY(angulo); //Metodo rotacion en el eje Y
   Cubo[2].translateZ(delta);
 
- 
-/*
+   /*
   La funcion tranlsate, se usa llamando al objeto en cuention. Posteriormente se  define el eje de tranlación. 
   En caso de ser en el eje X  ==> translateX 
   En caso de ser en el eje Y  ==> translateY 
   En caso de ser en el eje Z  ==> translateZ
   posteriormente se  abre un parentesis(), donde se indicara la longitud de la traslación 
 */
-
 
   //Luz (requerida para el material MeshLambertMaterial)
   light = new THREE.PointLight(0xffff00); //  Luz proveniente de un punto en el espacio,
@@ -110,6 +109,56 @@ function init() {
   // add the output of the renderer to the html element
   document.getElementById("webgl-output").appendChild(renderer.domElement);
 
-  // render the scene
-  renderer.render(scene, camera);
+/*Por otro metodo, haciendo uso del metodo promopt
+var angulo2 = prompt("Introduzca el angulo en grados(0°-90°):", "35");//Cuadro de dialogo que solicita angulo
+if (angulo2 == null || opcion == ""|| angulo2>90 || angulo2<0) {
+        alert ("ERROR");//Cuadro  de alerta
+        } 
+}
+
+*/
+  //Se inicializa el valor de los controles
+  var controls = new (function () {
+    this.rotation = 0;
+  })();
+
+  //Se llama a la libreria gui
+  var gui = new dat.GUI();
+  //Se agrega el control con sus limites desde 0 hasta 90 grados 
+  gui.add(controls, 'rotation', 0, 90);
+
+
+  //Cada vez que se cambia el valor en el control, se actualiza el programa 
+  render();
+
+  function render() {
+    var cub;
+   
+    //Se escoge uno de los tres cubos al azar por medio de Math.random
+    var ranCubo= Math.floor(Math.random() * 3); 
+    if (ranCubo == 0) {
+      cub = 0;
+    } else if (ranCubo == 1) {
+      cub = 1;
+    } else {
+      cub = 2;
+    }
+  
+   //Se escoge uno de los tres ejes al azar por medio de Math.random
+   var ranEje= Math.floor(Math.random() * 3); 
+    if (ranEje==0){
+      Cubo[cub].rotateX(controls.rotation * (Math.PI / 180));
+    }else if(ranEje==1){
+      Cubo[cub].rotateY(controls.rotation * (Math.PI / 180));
+    }else{
+      Cubo[cub].rotateZ(controls.rotation * (Math.PI / 180));
+    }
+
+   
+    // render the scene
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+    controls.rotation = 0;
+  }
+ 
 }
